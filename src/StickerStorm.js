a37df796be0c23da8db06d44418558544a77cd9e
Frankie Bruno <frankiebruno1991@gmail.com>
2023-09-13 15:46:55 -0500

    netlify8

--- src/StickerStorm.js
+++ src/StickerStorm.js
@@ -1,17 +1,27 @@
+import { Route, Routes } from "react-router-dom"
 import { NavBar } from "./components/nav/NavBar"
 import { ApplicationViews } from "./views/ApplicationViews"
 import { Authorized } from "./views/Authorized"
+import { Login } from "./components/auth/Login"
+import { Register } from "./components/auth/Register"
 
 
 
 
-export const StickerStorm = () => (
+export const StickerStorm = () => {
+    return <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    <Route path="*" element={
     <>
     <Authorized >
     <NavBar />
     <ApplicationViews />
     </Authorized>
     </>
-)
+
+    }/>
+    </Routes>
+}
 
 export default StickerStorm
--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -3,16 +3,16 @@ import { Authorized } from "./Authorized"
 import { StickerForm } from "../components/sticker/StickerForm"
 import { StickerList } from "../components/sticker/StickerList"
 import { StickerUpdate } from "../components/sticker/StickerUpdate"
-import { Login } from "../components/auth/Login"
-import { Register } from "../components/auth/Register"
+// import { Login } from "../components/auth/Login"
+// import { Register } from "../components/auth/Register"
 
 
 
 
 export const ApplicationViews = () => {
     return <> <Routes>
-            <Route path="/login" element={<Login to="/login" />} />
-            <Route path="/register" element={<Register />} />
+            {/* <Route path="/login" element={<Login to="/login" />} />
+            <Route path="/register" element={<Register />} /> */}
             <Route element={<Authorized />}>
             <Route path="/stickers" element={<StickerList />} />
             <Route path="/stickers/new" element={<StickerForm />} />
