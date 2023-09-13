aa7cde0e9670b9677c659d6654cb8013b4759fb7
Frankie Bruno <frankiebruno1991@gmail.com>
2023-09-13 15:37:13 -0500

    netlify7

--- src/views/Authorized.js
+++ src/views/Authorized.js
@@ -1,9 +1,13 @@
-import { Navigate, Outlet } from "react-router-dom"
+import { Navigate, useLocation } from "react-router-dom"
 
 
 export const Authorized = ({children}) => {
+  const location = useLocation()
   if (localStorage.getItem("st_token")) {
-    return <Outlet />
+    return children
   }
-  return <Navigate to='/login' replace />
+  return <Navigate to={`/login/${location.search}`} 
+  replace
+  state={{location}}
+  />
 }
