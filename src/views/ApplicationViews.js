4704758d3cb8cef2a8b56ca3d536af6a1ffcbe6d
Frankie Bruno <frankiebruno1991@gmail.com>
2023-09-18 15:52:43 -0500

    changes

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -16,13 +16,9 @@ export const ApplicationViews = () => {
             <Outlet/>
             </>
         }>
-            {/* <Route path="/login" element={<Login to="/login" />} />
-            <Route path="/register" element={<Register />} /> */}
-            <Route element={<Authorized />}>
             <Route path="stickers" element={<StickerList />} />
             <Route path="stickers/new" element={<StickerForm />} />
             <Route path="stickers/update/:stickerId" element={<StickerUpdate />} />
-            </Route>
             </Route>
         </Routes>
     )
