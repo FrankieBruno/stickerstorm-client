de761f25d04d7e8482dda102b22614abe971b902
Frankie Bruno <frankiebruno1991@gmail.com>
2023-09-18 15:47:28 -0500

    changes

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { Outlet, Route, Routes } from "react-router-dom"
 import { Authorized } from "./Authorized"
 import { StickerForm } from "../components/sticker/StickerForm"
 import { StickerList } from "../components/sticker/StickerList"
@@ -11,12 +11,18 @@ import { StickerUpdate } from "../components/sticker/StickerUpdate"
 
 export const ApplicationViews = () => {
     return (<Routes>
+        <Route path="/" element={
+            <>
+            <Outlet/>
+            </>
+        }>
             {/* <Route path="/login" element={<Login to="/login" />} />
             <Route path="/register" element={<Register />} /> */}
             <Route element={<Authorized />}>
-            <Route path="/stickers" element={<StickerList />} />
-            <Route path="/stickers/new" element={<StickerForm />} />
-            <Route path="/stickers/update/:stickerId" element={<StickerUpdate />} />
+            <Route path="stickers" element={<StickerList />} />
+            <Route path="stickers/new" element={<StickerForm />} />
+            <Route path="stickers/update/:stickerId" element={<StickerUpdate />} />
+            </Route>
             </Route>
         </Routes>
     )
