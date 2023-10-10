d408cd9cd75fd746ffc9a83ad521b1f12f0c039c
Frankie Bruno <frankiebruno1991@gmail.com>
2023-10-09 19:46:02 -0500

    login-update

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -20,7 +20,7 @@ export const Login = () => {
             .then(res => {
                 if ("valid" in res && res.valid && "token" in res) {
                     localStorage.setItem("st_token", res.token)
-                    navigate("/")
+                    navigate("/stickers/new")
                 }
                 else {
                     invalidDialog.current.showModal()
